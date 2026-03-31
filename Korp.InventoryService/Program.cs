eeed9f6a8a91cb991d351bc0a8e9f17e6376f65d
MarcosSilva-git <marcosjoseoliveira.mj@gmail.com>
2026-03-31 19:19:18 -0300

    feat: Added first migration in InventoryServiceDb

--- Korp.InventoryService/Program.cs
+++ Korp.InventoryService/Program.cs
@@ -3,10 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddProblemDetails();
@@ -19,6 +16,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    using var scope = app.Services.CreateScope();
+    var database = scope.ServiceProvider.GetRequiredService<InventoryDbContext>();
+    database.Database.Migrate();
+
     app.MapOpenApi();
 }
 
@@ -28,18 +29,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<InventoryDbContext>();
-    try
-    {
-        dbContext.Database.EnsureCreated();
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Erro ao criar banco de dados: {ex.Message}");
-        Console.WriteLine("Se o erro persistir, delete o arquivo estoque.db e reinicie o serviço.");
-    }
-}
 
 app.Run();
